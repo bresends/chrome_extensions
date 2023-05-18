14d5728e9ebd3c8007728ba53a505cab37a0c284
bruno <bruno.resende@gmx.com>
2023-05-18 19:58:08 -0300

    Feat: Added hide button

--- gpt_summarize/content.js
+++ gpt_summarize/content.js
@@ -56,7 +56,7 @@ async function handleChunkInput(
     }
 }
 
-function insertToTexBoxContainer(elements) {
+function insertElementsAboveTextBox(elements) {
     const textBoxContainer = document.querySelector(
         "textarea[tabindex='0']"
     )?.parentElement;
@@ -69,10 +69,58 @@ function insertToTexBoxContainer(elements) {
         elements.forEach((element) => {
             responseContainer.insertBefore(element.element, textBoxContainer);
         });
+
+        const closeEyeSVG = createOpenEyeSVG();
+
+        const openCloseBtn = createElement('button', {
+            id: 'open-close-btn',
+            className:
+                'absolute p-1 text-gray-500 enabled:dark:hover:text-gray-400',
+        });
+
+        openCloseBtn.appendChild(closeEyeSVG);
+
+        const sendText = document.querySelector(
+            "textarea[tabindex='0']"
+        )?.nextElementSibling;
+
+        sendText.parentElement.insertBefore(openCloseBtn, sendText);
     }
 }
 
-function insertElementsToDom(insertToTexBoxContainer) {
+function createOpenEyeSVG() {
+    // Create the SVG element
+    var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+    svg.setAttribute('fill', 'none');
+    svg.setAttribute('viewBox', '0 0 24 24');
+    svg.setAttribute('stroke-width', '1.5');
+    svg.setAttribute('stroke', 'currentColor');
+    svg.setAttribute('class', 'w-6 h-6');
+
+    // Create the first path element
+    var path1 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path1.setAttribute('stroke-linecap', 'round');
+    path1.setAttribute('stroke-linejoin', 'round');
+    path1.setAttribute(
+        'd',
+        'M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178'
+    );
+
+    // Create the second path element
+    var path2 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path2.setAttribute('stroke-linecap', 'round');
+    path2.setAttribute('stroke-linejoin', 'round');
+    path2.setAttribute('d', 'M15 12a3 3 0 11-6 0 3 3 0 016 0z');
+
+    // Append the path elements to the SVG element
+    svg.appendChild(path1);
+    svg.appendChild(path2);
+
+    return svg;
+}
+
+function createDomElements(insertToTexBoxContainer) {
     const progressBar = createElement('div', {
         className: 'progress-bar',
     });
@@ -165,7 +213,7 @@ function insertElementsToDom(insertToTexBoxContainer) {
 // Module: Main
 function initializeExtension() {
     const observer = new MutationObserver(() =>
-        insertElementsToDom(insertToTexBoxContainer)
+        createDomElements(insertElementsAboveTextBox)
     );
     observer.observe(document.body, { childList: true, subtree: true });
 }
